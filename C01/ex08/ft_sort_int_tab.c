8597325b2d78d279d61103341fefb1ee31b75924
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2024-08-19 18:19:52 +0100

    Add files via upload

---
+++ C01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raica-ba <raica-ba@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/18 17:07:25 by raica-ba          #+#    #+#             */
+/*   Updated: 2024/08/19 18:16:46 by raica-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+void	ft_sort_int_tab(int *tab, int size);
+void	ft_swap(int *a, int *b);
+
+void	ft_swap(int*a, int *b)
+{
+	int	c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	i;
+
+	while ()
+	{
+	
+	}
+}
+
+int	main(void)
+{
+
+}
