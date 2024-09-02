2cec5a3ff3f18367318dfb46f2f30e623de5ecd5
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2024-09-02 18:00:53 +0100

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,62 @@
+#include <unistd.h>
+
+int	ft_print_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		argv[i][j] = '\0';
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_swap(char**a, char **b)
+{
+	char	*c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] && s2[i]) && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc - 1)
+	{
+		j = 1;
+		while (j < argc - 1)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+				ft_swap(&argv[j], &argv[j + 1]);
+			j++;
+		}
+		i++;
+	}
+	ft_print_params(argc, argv);
+	return (0);
+}
