f544161def7d18006200fede87781ab706f22c68
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2024-09-02 14:04:33 +0100

    Add files via upload

---
+++ C07/ex00/ft_strdup.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+//#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	int			i;
+	char	*dest;
+
+	i = 0;
+	dest = (char *)malloc(sizeof(char) * ft_strlen(src + 1));
+	if (!dest)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	src[] = "afogar o ganso                                     a.";
+	printf("%s", ft_strdup(src));
+	return (0);
+}*/
