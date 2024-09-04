80a48b4c219a1139f7650935faa326f3dfc2077b
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2024-09-04 18:29:36 +0100

    Add files via upload

---
+++ C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raica-ba <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/04 18:24:51 by raica-ba          #+#    #+#             */
+/*   Updated: 2024/09/04 18:29:10 by raica-ba         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+#define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+void	ft_strlen(char *str);
+void	ft_strcmp(char *s1, char *s2);
+
+#endif
